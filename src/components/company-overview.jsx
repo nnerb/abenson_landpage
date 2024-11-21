3dbc79a3777a3e86b696e797762137270e8a9652
Brenn Santiago <brennaldwin.santiago@gmail.com>
2024-11-21 13:53:33 +0800

    update description with line breaks

--- src/components/company-overview.jsx
+++ src/components/company-overview.jsx
@@ -3,18 +3,37 @@ import vision from "../assets/images/vision.jpg"
 import core from "../assets/images/core.jpg"
 
 const details = [ 
-  { title: 'MISSION', description: 'At Abenson, our mission is to create JOY by bringing ‘Good Life’ to every Filipino home.' , image: mission },
-  { title: 'VISION', description: 'Happy Customer, Great People, Great Company.' , image: vision },
-  { title: 'CORE VALUES', description: 'Care for Customers, Leadership by All, Win-Win-Win!' , image: core }
+  { 
+    title: 'MISSION', 
+    description: 'At Abenson, our mission is to create JOY by bringing ‘Good Life’ to every Filipino home.' , 
+    image: mission 
+  },
+  { 
+    title: 'VISION',
+    description: `
+      Happy Customer, 
+      Great People, 
+      Great Company.
+    ` , 
+    image: vision 
+  },
+  { 
+    title: 'CORE VALUES', 
+    description: `
+      Care for Customers,
+      Leadership by All, 
+      Win-Win-Win!`,
+    image: core
+  }
 ]
 
 
 const CompanyOverview = () => {
   return ( 
-    <div className="p-4 w-full flex items-center justify-center">
-      <div className="inline-flex gap-2 overflow-auto scrollbar-hide cursor-pointer md:cursor-default" >
+    <section className="p-4 w-full flex items-center justify-center">
+      <ul className="inline-flex gap-2 overflow-auto scrollbar-hide cursor-pointer md:cursor-default" >
         {details.map((detail) => (
-          <div
+          <li
             key={detail.title}
             className="relative h-auto w-[401px] min-h-[188px] min-w-[177px] p-30"
           >
@@ -29,11 +48,19 @@ const CompanyOverview = () => {
               className="object-cover h-full w-full rounded-primary"
             />
             <h1 className="absolute top-[13px] left-[13px] md:top-[30px] md:left-[30px] text-[14px] lg:text-[28px] text-primary-gray font-poppins-medium">{detail.title}</h1>
-            <p className="absolute bottom-[13px] left-[13px] md:bottom-[30px] md:left-[30px] lg:text-2xl text-primary-text font-poppins-semibold text-xs">{detail.description}</p>
-          </div>
+            <p 
+              className="absolute bottom-[13px] left-[13px] right-[13px] text-primary-text font-poppins-semibold text-xs
+              md:bottom-[30px] md:left-[30px] md:right-[30px] lg:text-2xl"
+              dangerouslySetInnerHTML={{
+                __html: detail.title === "MISSION" 
+                ? detail.description
+                : detail.description.replace(/,/g, ',<br />')
+              }}
+            />
+          </li>
         ))}
-      </div>
-    </div>
+      </ul>
+    </section>
    );
 }
  

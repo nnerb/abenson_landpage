3945a04ee8e708bd4afeda6284238ebf5b7c57e6
Brenn Santiago <brennaldwin.santiago@gmail.com>
2024-11-25 02:54:55 +0800

    start creating footer accordion

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,6 +1,58 @@
 import Banner from "./components/banner";
 import Socials from "./components/socials";
 import Subscribe from "./components/subscribe";
+import CallIcon from "./icon/call-icon";
+import ChevronDownIcon from "./icon/chevron-down-icon";
+import TrailingIcon from "./icon/trailing-icon";
+
+const personalShopperDetails = [
+  { 
+    title: 'Online Personal Shopper',
+    items: [
+      '(02) 8811-8811',
+      '(02) 8811-8811',
+    ],
+    hasIcon: false,
+    icon: false
+  },
+  { 
+    title: 'In-Store Personal Shopper',
+    items: [''],
+    hasIcon: true,
+    icon: <TrailingIcon/>
+  },
+  { 
+    title: 'Overseas Hotlines',
+    items: [
+      '(02) 8248-3233',
+      '09989587101',
+      '(02) 8248-3233',
+      '09989587101',
+    ],
+    hasIcon: true,
+    icon: 'chevron'
+  },
+  { 
+    title: 'Business Solutions',
+    items: [
+      '(02) 8248-3233',
+      '09989587101',
+    ],
+    hasIcon: true,
+    icon: 'chevron'
+  },
+  { 
+    title: 'Online Personal Shopper',
+    items: [
+      '(02) 8248-3233',
+      '09989587101',
+      'customerservice@abenson.com',
+      'Customer Inquiry Form'
+    ],
+    hasIcon: true,
+    icon: 'chevron'
+  },
+]
 
 
 const Footer = () => {
@@ -28,6 +80,60 @@ const Footer = () => {
       </div>
       <div className="relative z-10 px-4 md:hidden">
         <Socials />
+      </div>
+      <div className="px-[15px] text-white relative z-10">
+        <ul className="last:border-b-[1px]">
+          <li className="py-4 px-2 flex flex-col gap-4 border-t-[1px]">
+            <div className="flex w-full items-center">
+              <h1 className="text-xs leading-[18px]">Online Personal Shopper</h1>
+              <div className="ml-auto">
+                <TrailingIcon />
+              </div>
+            </div>
+            <ul className="flex flex-col gap-2">
+              {personalShopperDetails.map((detail, index) => (
+                <li className="flex items-center gap-3" key={index}>
+                    <CallIcon />
+                  <p className="text-xs leading-[18px]">{detail.number}</p>
+                </li>
+              ))}
+                <li className="text-xs leading-[18px]">Open at 8am-6pm daily</li>
+                <li className="text-xs leading-[18px]">See more</li>
+            </ul>
+          </li>
+          <li className="py-4 px-2 flex flex-col gap-4 border-t-[1px]">
+            <div className="flex w-full items-center">
+              <h1 className="text-xs leading-[18px]">In-Store Personal Shopper</h1>
+              <div className="ml-auto">
+                <TrailingIcon />
+              </div>
+            </div>
+          </li>
+          <li className="py-4 px-2 flex flex-col gap-4 border-t-[1px]">
+            <div className="flex w-full items-center">
+              <h1 className="text-xs leading-[18px]">Overseas Hotlines</h1>
+              <div className="ml-auto">
+                <ChevronDownIcon />
+              </div>
+            </div>
+          </li>
+          <li className="py-4 px-2 flex flex-col gap-4 border-t-[1px]">
+            <div className="flex w-full items-center">
+              <h1 className="text-xs leading-[18px]">Business Solutions</h1>
+              <div className="ml-auto">
+                <ChevronDownIcon />
+              </div>
+            </div>
+          </li>
+          <li className="py-4 px-2 flex flex-col gap-4 border-t-[1px]">
+            <div className="flex w-full items-center">
+              <h1 className="text-xs leading-[18px]">Customer Care</h1>
+              <div className="ml-auto">
+                <ChevronDownIcon />
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
       {/* Overlay */}
       <div className="absolute inset-0 bg-primary-blue opacity-[93%]"/>
---
+++ src/components/footer/icon/chevron-down-icon.jsx
@@ -0,0 +1,9 @@
+const ChevronDownIcon = () => {
+  return ( 
+    <svg width="15" height="9" viewBox="0 0 15 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 1L7.5 7.5L14 1" stroke="#DADCE0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+   );
+}
+ 
+export default ChevronDownIcon;

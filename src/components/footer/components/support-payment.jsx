a0d6cfbbdb3ea9257e630643000f826c9b638055
Brenn Santiago <brennaldwin.santiago@gmail.com>
2024-11-25 20:18:55 +0800

    apply font works sans

--- src/components/footer/components/support-payment.jsx
+++ src/components/footer/components/support-payment.jsx
@@ -18,7 +18,7 @@ const SupportPayment = () => {
     },
     {
       title: 'Payment methods',
-      description: false
+      description: false,
     },
   ]
 
@@ -60,42 +60,50 @@ const SupportPayment = () => {
             </div>
           ))}
         </div>
-        <div className="flex gap-[75px] lg:flex-col lg:gap-[52px] ">
-          {supportPaymentDetails.slice(2).map((detail, index) => (
-            <div 
-              key={index} 
-              className="
-                flex flex-col items-center
-                text-xs min-w-[92px] w-[112px]
-                gap-5 lg:items-start lg:gap-4
-                lg:w-[151px]
-              "
-            >
-              <h1 
+        <div className="flex gap-[75px] lg:flex-col lg:gap-[52px]">
+          {supportPaymentDetails.slice(2).map((detail, index) => {
+            const isLastElement = index === supportPaymentDetails.slice(2).length - 1;
+
+            return (
+              <div 
+                key={index} 
                 className="
-                text-primary-yellow font-poppins-semibold
-                  leading-6 text-wrap
-                  lg:text-base lg:leading-[32px]
+                  flex flex-col items-center
+                  text-xs min-w-[92px] w-[112px]
+                  gap-5 lg:items-start lg:gap-4
+                  lg:w-[151px]
                 "
               >
-                {detail.title}
-              </h1>
-             
-              { detail.description 
-                ? <p 
-                    className="
-                    text-white text-center 
-                      lg:text-start
-                      lg:text-xs lg:leading-[18px]
-                      lg:max-w-[92px]
-                    "
-                  > 
-                    {detail.description}
-                  </p> 
-                : <PaymentImagesGrid />
-               }
-            </div>
-          ))}
+                <h1 
+                  className={`
+                    text-primary-yellow leading-6 text-wrap
+                    lg:text-base lg:leading-[32px]
+                    ${
+                      isLastElement 
+                        ? "md:font-works-sans-semibold lg:font-poppins-semibold" 
+                        : "font-poppins-semibold"
+                    }
+                  `}
+                >
+                  {detail.title}
+                </h1>
+
+                {detail.description 
+                  ? <p 
+                      className="
+                        text-white text-center 
+                        lg:text-start
+                        lg:text-xs lg:leading-[18px]
+                        lg:max-w-[92px]
+                      "
+                    > 
+                      {detail.description}
+                    </p> 
+                  : <PaymentImagesGrid />
+                }
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,18 +5,15 @@ import Subscribe from "./components/subscribe";
 import SupportPayment from "./components/support-payment";
 
 const Footer = () => {
- 
 
   return (
     <footer
       className="
-        relative h-[full] bg-no-repeat bg-footer-image flex flex-col gap-6
-        bg-sm-position bg-sm-bg-size md:gap-5
-        md:bg-md-position md:bg-md-bg-size
-         lg:py-[58px] lg:px-4
-         xl:px-[70px]
-        lg:flex-row lg:items-start lg:justify-between z-20
-        
+        relative h-full bg-no-repeat bg-footer-image 
+        flex flex-col gap-6 bg-sm-position bg-sm-bg-size 
+        md:gap-5 md:bg-md-position md:bg-md-bg-size
+        lg:py-[58px] lg:px-4 lg:flex-row lg:items-start lg:justify-between 
+        xl:px-[70px]
       "
     >
       <div 
@@ -35,6 +32,7 @@ const Footer = () => {
       <div className="px-[15px] lg:px-0 text-white relative z-10 md:px-[38px] flex flex-col gap-[21px]">
         <h1
           className="
+            hidden lg:block
           text-primary-yellow font-poppins-semibold
             leading-6  
             lg:text-base lg:leading-[32px]
@@ -42,7 +40,9 @@ const Footer = () => {
         >
           Connect with us
         </h1>
-        <Socials />
+        <div className="hidden lg:block">
+          <Socials />
+        </div>
         <Contacts />
       </div>
       {/* Overlay */}

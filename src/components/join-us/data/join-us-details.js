348e21a06f409a883964afd6e5ea90e7c66ff8ab
Brenn Santiago <brennaldwin.santiago@gmail.com>
2024-11-26 03:45:41 +0800

    flix join us component

---
+++ src/components/join-us/data/join-us-details.js
@@ -0,0 +1,37 @@
+// SVG icons
+
+import AppliancesIcon from "../icons/appliances-icon";
+import AwesomeIcon from "../icons/awesome-icon";
+import LeaderIcon from "../icons/leader-icon";
+import LifeLongIcon from "../icons/lifelong-icon";
+import StrongIcon from "../icons/strong-icon";
+
+
+export const joinUsDetails = [
+  { 
+    title: '#1 in Appliances', 
+    description: 'Grow your career with the Leader. Let’s bring happiness together through technology.',
+    icon: AppliancesIcon,
+  },
+  { 
+    title: 'Awesome Benefits', 
+    description: 'Enjoy good compensation package and exciting benefits.',
+    icon: AwesomeIcon
+  },
+  { 
+    title: 'Strong Values Culture', 
+    description: 'Values are important to us. Service and Leadership guides our culture.',
+    icon: StrongIcon
+  },
+  { 
+    title: 'A Leader in Digital', 
+    description: 'Abenson.com is the number one Filipino eRetailer. We are a winning team that continues to innovate.',
+    icon: LeaderIcon
+  },
+  { 
+    title: 'Lifelong Learning ', 
+    description: 'We believe that if you grow, we grow. We have an industry leading career growth and retention rates.',
+    icon: LifeLongIcon
+  },
+
+]
